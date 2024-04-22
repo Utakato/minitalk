dab0494951697484c5e366b7373dcbbf1cc306d0
Vasile Schipor <vschipor@uw.co.uk>
2024-04-22 14:31:06 +0200

    add signal handler and infinite loop

--- server.c
+++ server.c
@@ -1,10 +1,23 @@
 #include "minitalk.h"
 
+
+void signal_handler(int signum)
+{
+    if (signum == SIGUSR1)
+        write(1, "0", 1);
+    else if (signum == SIGUSR2)
+        write(1, "1", 1);
+}
+
 int main (void) 
 {
     pid_t pid;
 
     pid = getpid();
     printf("%u" , pid);
+    signal(SIGUSR1, signal_handler);
+    while (1)
+        continue;
+    
     return (0);
 }
