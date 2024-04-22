ac8fb0f345b70be6174889ae80ac10f0c72ad51f
Vasile Schipor <vschipor@uw.co.uk>
2024-04-22 17:31:23 +0200

    test for second signal

--- server.c
+++ server.c
@@ -14,8 +14,10 @@ int main (void)
     pid_t pid;
 
     pid = getpid();
-    printf("%u" , pid);
+    printf("the process id oasdasdf the server is: %u\n" , pid);
+
     signal(SIGUSR1, signal_handler);
+    signal(SIGUSR2, signal_handler);
     while (1)
         continue;
     
