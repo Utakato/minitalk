0f148801aab019c5b94d0bdc300994066e5fcfae
Vasile Schipor <vschipor@uw.co.uk>
2024-04-22 14:21:47 +0200

    add minitalk

---
+++ minitalk.h
@@ -0,0 +1,16 @@
+#ifndef MINITALK_H
+ #define MINITALK_H
+
+#ifndef SIGUSR1
+ #define SIGUSR1 10
+#endif
+
+#ifndef SIGUSR2
+ #define SIGUSR2 12
+#endif
+
+#include <unistd.h>
+/* testing */
+#include <stdio.h>
+
+#endif
