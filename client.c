c54e8e1fae0cdcecbd4ea139dcecfff9d59a6bd7
Vasile Schipor <vschipor@uw.co.uk>
2024-05-09 16:20:29 +0300

    breakdown chars into bits to send to server

--- client.c
+++ client.c
@@ -1,8 +1,35 @@
 #include "minitalk.h"
 #include <stdlib.h>
 
+// 01100001 = a
+void check_bits(char letter, int pid) {
+    int is_one;
+    int i = 0;
+    
+    printf("letter is: %i \n", letter);
+    while (i < 7)
+    {
+        is_one = letter & '@';
+        if (is_one == '@')
+        {   
+            printf("sent 1\n");
+            kill(pid, SIGUSR1);
+        }
+        else {
+            printf("sent 0\n");
+            kill(pid, SIGUSR2);
+        }
+        letter = letter << 1;
+        i++;
+        // usleep(100);
+    }
+    
+}
+
+
 int main (int argc,char * argv[] ) {
     int pid ;
+    int i;
     char *message ;
 
     if (argc != 3)
@@ -11,13 +38,17 @@ int main (int argc,char * argv[] ) {
         return (1);
     }
 
+    i = 0;
     pid = atoi(argv[1]);
     message = argv[2];
 
     printf("%s\n", message);
-
-    kill(pid, SIGUSR1);
-    kill(pid, SIGUSR2);
+    while (message[i])
+    {
+        check_bits(message[i], pid);
+        i++;
+    }
 
     return (0);
 }
+
