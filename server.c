77890728f77c6be8a401ba60b0e8647cfd4668b3
Vasile Schipor <vschipor@uw.co.uk>
2024-05-09 16:20:59 +0300

    add signal handler that receives bits and constructs a char

--- server.c
+++ server.c
@@ -3,15 +3,26 @@
 
 void signal_handler(int signum)
 {
-    if (signum == SIGUSR1)
-        write(1, "0", 1);
-    else if (signum == SIGUSR2)
-        write(1, "1", 1);
+    static char letter;
+    static int i;
+
+    if (i <= 6) {
+        letter = letter << 1;
+        if (signum == SIGUSR1)
+            letter = letter | 1;
+        i++;
+    } 
+    if (i == 7) {
+        write(1, &letter, 1);
+        letter = 0;
+        i = 0;
+    }
 }
 
-int main (void) 
+int main (void)
 {
     pid_t pid;
+    
 
     pid = getpid();
     printf("the process id oasdasdf the server is: %u\n" , pid);
